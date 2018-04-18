049e06ceed84e925121efc2f0ff931767249e250
Denis Kozyrev <deniskozyrev55@gmail.com>
2018-04-18 17:26:34 +0300

    shirt cards and game level buttons functional

--- js/index.js
+++ js/index.js
@@ -1,47 +1,71 @@
-let greetingFirstNameInput = document.querySelector('#greetingFirstName');
+// game Player Profile form register and input game rules page greeting
+let gamePlayerProfilePage = document.querySelector('#gamePlayerProfilePage');
 
-let greetingLastNameInput = document.querySelector('#greetingLastName');
+let greetingFirstNameInput = document.querySelector('#playerProfileGreetingFirstName');
 
-let fullNameGreeting = document.querySelector('.player-full-name-greeting');
+let greetingLastNameInput = document.querySelector('#playerProfileGreetingLastName');
 
-let playerProfileButton = document.querySelector('.player-profile-register-button');
+let fullNameGreeting = document.querySelector('#fullNameGreeting');
 
-let gameGreeting = document.querySelector('.game-greeting-and-player-profile-page');
+let playerProfileRegisterButton = document.querySelector('#playerProfileRegisterButton');
 
-let gameRules = document.querySelector('.player-greeting-and-game-rules-page');
+let gameRulesPage = document.querySelector('.player-greeting-and-game-rules-page');
 
 function getFullName() {
-    gameGreeting.style.display = 'none';
-    gameRules.style.display = 'block';
+    gamePlayerProfilePage.style.display = 'none';
+    gameRulesPage.style.display = 'block';
     fullNameGreeting.innerHTML = "Hello" + " " + greetingFirstNameInput.value + " " + greetingLastNameInput.value + "!";
 }
 
-playerProfileButton.addEventListener('click', getFullName)
+playerProfileRegisterButton.addEventListener('click', getFullName)
 
 
 
-
+// shirt Button options
 let shirtOptionsButton = document.querySelector('#shirtCardsButton');
 
 let shirtButtonDropdownContent = document.querySelector('#shirtCardsButtonContent')
 
-let shirtCardsImg = document.querySelectorAll('.shirt-cards-img');
-
-// let yodaImg = document.querySelector('.yoda-img');
-
 shirtOptionsButton.addEventListener('click', () => {
-    // yodaImg.style.display = 'none';
     shirtButtonDropdownContent.style.display = 'block';
 });
 
-function shirtButtonsOptions() {
+// Добовляет нужную рубашку кард при выборе игрока и убирает внутренний блок 
+let gameCardBlock = document.getElementsByClassName('front');
+
+let yodaShirtCard = document.querySelector('.yoda');
+let vaderShirtCard = document.querySelector('.vader');
+let r2d2ShirtCard = document.querySelector('.r2d2');
+
+function shirtButtonDropdownContentOff() {
     shirtButtonDropdownContent.style.display = 'none';
-    // yodaImg.style.display = 'block';
 }
 
-shirtCardsImg.forEach(img => img.addEventListener('click', shirtButtonsOptions));
+yodaShirtCard.addEventListener('click', () => {
+    Array.from(gameCardBlock).forEach(card => {
+        card.innerHTML = '<img class = "card-img" src = "../Match-Match-Game/images/yoda-img.png" width = "130px" height = "130px" >';
+    });
+    shirtButtonDropdownContentOff();
+});
+
+vaderShirtCard.addEventListener('click', () => {
+    Array.from(gameCardBlock).forEach(card => {
+        card.innerHTML = '<img class = "card-img" src = "../Match-Match-Game/images/darth-vader-img.png" width = "130px" height = "130px" >';
+    });
+    shirtButtonDropdownContentOff();
+});
+
+r2d2ShirtCard.addEventListener('click', () => {
+    Array.from(gameCardBlock).forEach(card => {
+        card.innerHTML = '<img class = "card-img" src = "../Match-Match-Game/images/r2d2-img.png" width = "130px" height = "130px" >';
+    });
+    shirtButtonDropdownContentOff();
+});
 
 
+
+
+// game Level event (открывает внутренний блок с выбором сложности игры)
 let gameLevelButton = document.querySelector('#gameLevelButton');
 
 let levelButtonContent = document.querySelector('#levelButtonContent');
@@ -50,10 +74,48 @@ gameLevelButton.addEventListener('click', () => {
     levelButtonContent.style.display = 'block';
 });
 
+// генерирует нужное количество кард и убирает внутренний блок level button 
+let gameLevel3x2 = document.querySelector('#gameLevel3x2');
+let gameLevel6x3 = document.querySelector('#gameLevel6x3');
+let gameLevel8x3 = document.querySelector('#gameLevel8x3');
+let gameFild = document.querySelector('#gameFild');
+let gameCard = document.querySelector('#gameCard');
+
+gameLevel3x2.addEventListener('click', () => {
+    let cardNumber = 6
+    for (let i = 0; i < cardNumber - 1; i++) {
+        let gameCard2 = gameCard.cloneNode(true);
+        gameCard.parentNode.insertBefore(gameCard2, gameCard.nextSibling);
+    }
+    gameFild.style.width = '480px';
+    levelButtonContent.style.display = 'none';
+});
+
+gameLevel6x3.addEventListener('click', () => {
+    let cardNumber = 18
+    for (let i = 0; i < cardNumber - 1; i++) {
+        let gameCard2 = gameCard.cloneNode(true);
+        gameCard.parentNode.insertBefore(gameCard2, gameCard.nextSibling);
+    }
+    gameFild.style.width = '960px';
+    levelButtonContent.style.display = 'none';
+});
+
+gameLevel8x3.addEventListener('click', () => {
+    let cardNumber = 24
+    for (let i = 0; i < cardNumber - 1; i++) {
+        let gameCard2 = gameCard.cloneNode(true);
+        gameCard.parentNode.insertBefore(gameCard2, gameCard.nextSibling);
+    }
+    gameFild.style.width = '1285px';
+    levelButtonContent.style.display = 'none';
+});
+
+//new game button event
 let newGameButton = document.querySelector('#newGameButton');
 let gamePage = document.querySelector('.game-page');
 
 newGameButton.addEventListener('click', () => {
-    gameRules.style.display = 'none';
+    gameRulesPage.style.display = 'none';
     gamePage.style.display = 'block';
 });
